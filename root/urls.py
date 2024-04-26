26809e1b6b5bff0276f09f8b0d6e1baaf60474ba
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-04-26 12:24:27 +0330

    create view and urls

---
+++ root/urls.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+    path('', home),
+    path('contact/', contact),
+    path('about/', about),
+]
---
+++ root/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'root/index.html')
+
+
+def contact(request):
+    return render(request, "root/contact.html")
+
+
+def about(request):
+    return render(request, "root/about.html")
+
+# Create your views here.
