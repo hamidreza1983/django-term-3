ca6e5241736f5d751e913a09a4795535219c1391
“hamidreza1983” <hamidrezacoder1983@gmail.com>
2024-08-23 17:17:33 +0330

    edit view

--- config/settings.py
+++ config/settings.py
@@ -153,5 +153,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
