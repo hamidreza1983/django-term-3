9237f729b2633e8ebd6ca4b0c319c6ae9339b138
“hamidreza1983” <hamidrezacoder1983@gmail.com>
2024-10-10 17:22:15 +0330

    create unittest

--- root/tests.py
+++ root/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import (HomeView, 
+                    AboutView, 
+                    contact)
 
-# Create your tests here.
+from .forms import ContactUsForm
+
+class TestUrl(TestCase):
+    """
+    def test_url_home(self):
+        url = reverse("root:home")
+        self.assertEqual(resolve(url).func.view_class, HomeView)
+
+    def test_url_contact(self):
+        url = reverse("root:contact")
+        self.assertEqual(resolve(url).func, contact)
+    """
+
+    def test_form_true_contact(self):
+        form = ContactUsForm(data={
+            "name"  : "hamid",
+            "email" : "absc@test.com",
+            "subject" : "test",
+            "message" : "test"
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_form_false_contact(self):
+        form = ContactUsForm(data={
+            "name"  : "hamid",
+            "email" : "abscest.com",
+            "subject" : "test",
+            "message" : "test"
+        })
+        self.assertFalse(form.is_valid())
+
+
+
+
--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import ServiceView, ServiceDetaiView
 
-# Create your tests here.
+class TestUrl(TestCase):
+    def test_url_services(self):
+        url = reverse("services:services")
+        self.assertEqual(resolve(url).func.view_class, ServiceView)
+
+    def test_url_service_detail(self):
+        url = reverse("services:services-detail", kwargs={"pk":150})
+        self.assertEqual(resolve(url).func.view_class, ServiceDetaiView)
+
+    def test_url_service_by_category(self):
+        url = reverse("services:list_by_category", kwargs={"category":"test"})
+        self.assertEqual(resolve(url).func.view_class, ServiceView)
