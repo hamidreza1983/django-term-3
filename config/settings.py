c89ba7065ef8da9811a5caf15358e7801ddc33c7
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-10-19 00:16:09 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/5.0/ref/settings/
+https://docs.djangoproject.com/en/5.0/ref/settings/.
 """
 
 from pathlib import Path
